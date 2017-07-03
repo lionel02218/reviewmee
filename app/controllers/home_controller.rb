473ad9df6c7794dc2b2e88b0fb1f8feb4c6c3d06
lionel <lionel02218@yahoo.com>
2017-07-03 20:00:19 +0800

    Fxied issues of invision comments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,10 +7,10 @@ class HomeController < ApplicationController
     if user.present?
       @subscribed = false
     else
-      pswd = (0...10).map { ('a'..'z').to_a[rand(26)] }.join
-      user = User.create(email:params[:email], password: pswd, password_confirmation: pswd)
       gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
       gibbon.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {email_address: params[:email], status: "subscribed"})
+      pswd = (0...10).map { ('a'..'z').to_a[rand(26)] }.join
+      user = User.create(email:params[:email], password: pswd, password_confirmation: pswd)      
       @subscribed = true
     end
     respond_to do |format|
